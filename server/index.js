0970196bcb1f704a83e8d8b63da68e9268d64041
Coty Janeway <Coty.Janeway@gmail.com>
2023-07-11 16:34:36 -0700

    Flesh out routes more

--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const morgan = require("morgan");
 const PORT = process.env.PORT || 9999;
 
+// Middleware
+app.use(morgan('tiny'));
+app.use(express.json);
 
 // GET ALL
 app.get('/api/v1/restaurants', (req, res) => {
   console.log('GET ALL Restaurants');
+  // console.log(req.params.id);
+
+
+  res.status(200).send();
 
   try {
   //
@@ -19,9 +27,10 @@ app.get('/api/v1/restaurants', (req, res) => {
 // GET ONE
 app.get('/api/v1/restaurants/:id', (req, res) => {
   console.log('GET ALL Restaurants');
+  console.log(req.params);
 
   try {
-  //
+  res.status(200).send();
   } catch (err) {
     console.log(err);
   }
@@ -32,6 +41,8 @@ app.get('/api/v1/restaurants/:id', (req, res) => {
 // POST
 app.post('/api/v1/restaurants', (req, res) => {
   console.log('Post a restaurant');
+  console.log(req.body);
+
 
   try {
     //
@@ -44,6 +55,10 @@ app.post('/api/v1/restaurants', (req, res) => {
 // UPDATE
 app.put('/api/v1/restaurants', (req, res) => {
   console.log('Update a restaurant');
+  console.log(req.params);
+  console.log(req.body);
+
+
 
   try {
   //
@@ -58,7 +73,7 @@ app.delete('/api/v1/restaurants', (req, res) => {
   console.log('Delete a restaurant');
 
   try {
-  //
+  res.status(204).send();
   } catch (err) {
     console.log(err);
   }
