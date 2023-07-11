6cb2139c195b2418c4c0bd3d371a7fe4f960d817
Coty Janeway <Coty.Janeway@gmail.com>
2023-07-11 16:20:07 -0700

    Add basic routes

--- server/index.js
+++ server/index.js
@@ -1,7 +1,69 @@
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 9999;
+
+
+// GET ALL
+app.get('/api/v1/restaurants', (req, res) => {
+  console.log('GET ALL Restaurants');
+
+  try {
+  //
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+
+// GET ONE
+app.get('/api/v1/restaurants/:id', (req, res) => {
+  console.log('GET ALL Restaurants');
+
+  try {
+  //
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+
+
+// POST
+app.post('/api/v1/restaurants', (req, res) => {
+  console.log('Post a restaurant');
+
+  try {
+    //
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+
+// UPDATE
+app.put('/api/v1/restaurants', (req, res) => {
+  console.log('Update a restaurant');
+
+  try {
+  //
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+
+// DELETE
+app.delete('/api/v1/restaurants', (req, res) => {
+  console.log('Delete a restaurant');
+
+  try {
+  //
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 app.listen(PORT, () => {
-  console.log(`\nServer is listening on port ${PORT} \n`)
-})
+  console.log(`\n ~~ Server is listening on port ${PORT} ~~ \n`);
+});
