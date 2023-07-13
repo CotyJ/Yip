c55d73e2644434e68bb74402f798110e53edc603
Coty Janeway <Coty.Janeway@gmail.com>
2023-07-12 17:42:44 -0700

    Add context component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,32 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import Home from "./routes/Home";
-import RestaurantDetailPage from "./routes/RestaurantDetailPage";
-import UpdatePage from "./routes/UpdatePage";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import RestaurantDetailPage from './routes/RestaurantDetailPage';
+import UpdatePage from './routes/UpdatePage';
+import { RestaurantContextProvider } from './context/RestaurantContext';
 
 const App = () => {
-
-  return(
-  <div>
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<Home/>}/>
-        <Route exact path="/restaurants/:id/update" element={<UpdatePage/>}/>
-        <Route exact path="/restaurants/:id" element={<RestaurantDetailPage/>}/>
-      </Routes>
-    </Router>
-  </div>
-  )
-}
+  return (
+    <RestaurantContextProvider>
+      <div className="container">
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route
+              exact
+              path="/restaurants/:id/update"
+              element={<UpdatePage />}
+            />
+            <Route
+              exact
+              path="/restaurants/:id"
+              element={<RestaurantDetailPage />}
+            />
+          </Routes>
+        </Router>
+      </div>
+    </RestaurantContextProvider>
+  );
+};
 
 export default App;
