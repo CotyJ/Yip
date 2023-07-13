ca3a28aa058f32af7d490f787d27a05d0438339d
Coty Janeway <Coty.Janeway@gmail.com>
2023-07-12 17:44:08 -0700

    Add useEffect

--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -1,38 +1,68 @@
-import React from "react";
+import React, { useContext, useEffect } from 'react';
+import RestaurantFinder from '../apis/RestaurantFinder';
+import { RestaurantContext } from '../context/RestaurantContext';
 
-const RestaurantList = () => {
+const RestaurantList = (props) => {
+  const { restaurants, setRestaurants } = useContext(RestaurantContext);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await RestaurantFinder.get('/');
+        setRestaurants(response.data.data.restaurants); //may need 1 less .data
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, []);
 
-  return(
-  <div className="list-group">
-<thead>
-  <tr className="bg-primary">
-    <th scope="col">Restaurant</th>
-    <th scope="col">Location</th>
-    <th scope="col">Price Range</th>
-    <th scope="col">Rating</th>
-    <th scope="col">Edit</th>
-    <th scope="col">Delete</th>
-  </tr>
-</thead>
+  return (
+    <div className="list-group">
+      <table className="table table-hover table-dark">
+        <thead>
+          <tr className="bg-primary">
+            <th scope="col">Restaurant</th>
+            <th scope="col">Location</th>
+            <th scope="col">Price Range</th>
+            <th scope="col">Rating</th>
+            <th scope="col">Edit</th>
+            <th scope="col">Delete</th>
+          </tr>
+        </thead>
 
-<tbody>
-  <tr>
-    <td>MdDonalds</td>
-    <td>New York</td>
-    <td>$</td>
-    <td>Rating</td>
-    <td>
-      <button className="btn btn-warning">Update</button>
-    </td>
-    <td>
-      <button className="btn btn-danger">Delete</button>
-    </td>
-  </tr>
-</tbody>
-
-    <table className="table table-hover table-dark"></table>
-  </div>
-  )
-}
+        <tbody>
+          {restaurants && restaurants.map((restaurant) => {
+            return (
+              <tr key={restaurant.id}>
+                <td>{restaurant.name}</td>
+                <td>{restaurant.location}</td>
+                <td>{'$'.repeat(restaurant.price_range)}</td>
+                <td>reviews</td>
+                <td>
+                  <button className="btn btn-warning">Update</button>
+                </td>
+                <td>
+                  <button className="btn btn-danger">Delete</button>
+                </td>
+              </tr>
+            );
+          })}
+          {/* <tr>
+            <td>MdDonalds</td>
+            <td>New York</td>
+            <td>$</td>
+            <td>Rating</td>
+            <td>
+              <button className="btn btn-warning">Update</button>
+            </td>
+            <td>
+              <button className="btn btn-danger">Delete</button>
+            </td>
+          </tr> */}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default RestaurantList;
