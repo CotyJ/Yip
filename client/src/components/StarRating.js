d09926d05cf41ddbd160135bc9ee5b1873256e60
Coty Janeway <Coty.Janeway@gmail.com>
2023-07-14 17:12:32 -0700

    Add star rating component

---
+++ client/src/components/StarRating.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const StarRating = ({rating}) => {
+  const stars = [];
+  const solidStar = <i class="fa-solid fa-star text-warning"></i>;
+  const emptyStar = <i class="fa-regular fa-star text-warning"></i>;
+  const halfStar = <i class="fa-solid fa-star-half-stroke text-warning"></i>;
+
+  for (let i = 1; i <= 5; i++) {
+    if (i <= rating) {
+      stars.push(solidStar);
+    } else if (i === Math.ceil(rating) && !Number.isInteger(rating)) {
+      stars.push(halfStar)
+    } else {
+      stars.push(emptyStar);
+    }
+  }
+
+
+  return (
+    <>
+      {stars}
+    </>
+  )
+}
+
+export default StarRating;
